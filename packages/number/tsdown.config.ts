d6f9e23aef4172509702c35f6574db94f0d366bd
meluiz <me.luizfelipe@gmail.com>
2026-09-03 19:10:18 -0300

    feat(number): initial commit from developer

---
+++ packages/number/tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  dts: true,
+  clean: true,
+  unbundle: true,
+  treeshake: true,
+  format: ['esm'],
+  attw: 'ci-only',
+  publint: 'ci-only',
+  entry: ['src/index.ts'],
+});
